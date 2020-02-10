52265037259708676fe906e485e62d8bad568952
Arnaud <arnaud.ruymaekers@openline.nl>
2020-02-10 14:59:10 +0100

    commented install file

--- install.ps1
+++ install.ps1
@@ -1,12 +1,15 @@
+# Basic setup
 Set-Location ($PSScriptRoot + "\src")
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
-
 $Exercise_folder = $DesktopPath + "\Turtle_exercise"
 
+# Compilation of java file to the exercise folder
 javac -d $Exercise_folder *.java
 
+# Copy of java files
 Copy-Item ($PSScriptRoot + "\src\*.*") -Destination $Exercise_folder
 
+# Hiding background files
 attrib +h ($Exercise_folder + "\turtle")
 attrib +h ($Exercise_folder + "\Main.class")
 attrib +h ($Exercise_folder + "\Main.java")
